7f4991f98a58d2a3f920021987d66564f94e88ef
Sebastian <jsortiz82@misena.edu.co>
2023-12-24 15:22:56 -0500

    se sube mensaje

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('revealButton').addEventListener('click', function() {
+    document.getElementById('specialMessage').classList.toggle('hidden');
+    document.getElementById('letterContainer').classList.toggle('open');
+});
